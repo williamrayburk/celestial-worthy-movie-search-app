e5f0694339309bf254643bb5b38c5eff11aa4441
williamrayburk20 <williamrayburk20@gmail.com>
2021-02-16 21:33:49 -0700

    Added comments

--- webservice/myapp/app.js
+++ webservice/myapp/app.js
@@ -1,3 +1,7 @@
+//The code below is from https://expressjs.com/en/starter/hello-world.html and was an attempt to spin up
+//a Web Server displaying Hello World.  First attempt failed.  Will try again.  Will focus on Pseudode items
+//below first.
+
 const express = require('express')
 const app = express()
 const port = 3000
@@ -9,3 +13,12 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+//Main TODOs:
+//Pseudocode main method to start an embedded server
+//  Recommendation to define the API specs first using Postman
+//Pseudocode back end service call to TMDB API 
+//Pseudocode return of results from back end to web service through REST API 
+//Pseudocode unit tests using Mocha
+
+
